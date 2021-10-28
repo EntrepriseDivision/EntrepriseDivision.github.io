dcf115cd53754fe94ac5205902aa3af3b087f621
Victoria FitzGibbon <90334128+VictoriaFitz@users.noreply.github.com>
2021-10-28 15:28:31 +0000

    html

--- jeux/script.js
+++ jeux/script.js
@@ -19,35 +19,6 @@ message1 += " " + (num1 / num2);
 var affiche = document.getElementById("divAffiche1");
         var message = "<br> La reponse est:" + message1 + " "
             affiche.innerHTML=message; 
-
-var radiosReponse = document.getElementsByName("rad1");
-       var messageReponseOperation = "";
-       var messagePoint
-       for (var i = 0; i < radiosReponse.length; i++) {
-           if (radiosReponse[i].checked) {
-               reponseOperation = radiosReponse[i].value;
-           } 
-       } 
-           if(radiosReponse[0].checked==true && randomNumber1 == 1) {
-            messageReponseOperation += "Bon reponse!";
-            messagePoint += " " + 1;
-           } else if (radiosReponse[1].checked==true && randomNumber1 == 2) {
-             messageReponseOperation += "Bon reponse!";
-             messagePoint += " " + 1;
-           } else if (radiosReponse[2].checked == true && randomNumber1 == 3) {
-            messageReponseOperation += "Bon reponse!";
-            messagePoint += " " + 1;
-           } else if (radiosReponse[3].checked == true && randomNumber1 == 4) {
-            messageReponseOperation += "Bon reponse!";
-            messagePoint += " " + 1;
-           } else {
-            messageReponseOperation += "Mauvais reponse!"
-            messgaePoint += " " + 0;
-           }
-
-       var afficheReponse = document.getElementById("divAffiche2");
-        var message2 = "<br>  + messageReponseOperation + " "
-            afficheReponse.innerHTML=message2;  
 }
 
 
