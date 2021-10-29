58b747ecc34ffa3a13b3c2bcbfb48a4851abdf1e
Victoria FitzGibbon <90334128+VictoriaFitz@users.noreply.github.com>
2021-10-29 17:12:08 +0000

    css index.html

--- jeux/script.js
+++ jeux/script.js
@@ -1,5 +1,5 @@
-
-function calcule() {
+ var messagePoint = "";
+ document.getElementById("btnQ1").onclick = function calcule() {
 var message1 = ""
 var num1 = parseInt(document.getElementById("num1Q1").value);
 var num2 = parseInt(document.getElementById("num2Q1").value);
@@ -17,8 +17,57 @@ message1 += " " + (num1 / num2);
 ("Rien selecter");
 }
 var affiche = document.getElementById("divAffiche1");
-        var message = "<br> La reponse est:" + message1 + " "
+        var message = "<div id='reponq1'><strong> La reponse est:" + message1 + " </strong></div>"
             affiche.innerHTML=message; 
-}
+
+
+ document.getElementById("btn2Q1").onclick = function calcule2(){ 
+ var radioOperation1 = document.getElementsByName("rad1");
+ var message2 = "";
+ var operation1 = "";
+       for (var i = 0; i < radioOperation1.length; i++) {
+           if (radioOperation1[i].checked) {
+               operation1 = radioOperation1[i].value;
+           } 
+       } 
+        
+     if (radioOperation1[0].checked == true && randomNumber1 == 1) {
+    message2 += "Bonne réponse";
+    messagePoint += 1;
+     } else if (radioOperation1[1].checked == true && randomNumber1 == 2) {
+    message2 += "Bonne réponse";
+    messagePoint += 1;
+     } else if (radioOperation1[2].checked == true && randomNumber1 == 3) {
+    message2 += "Bonne réponse";
+    messagePoint += 1;
+     } else if (radioOperation1[3].checked == true && randomNumber1 == 4) {
+    message2 += "Bonne réponse";
+    messagePoint += 1;
+     } else {
+    message2 += "Mauvaise réponse";
+    messagePoint += 0;
+     }
+
+    var affiche2 = document.getElementById("divAffiche2");
+    if (message2 == "Bonne réponse") {
+        var message3 =  "<div id='bonneRep'><strong>" + message2 + "<br>  Votre pointage est " + messagePoint + ".</strong> </div>"
+            affiche2.innerHTML=message3; 
+    } else if (message2 == "Mauvaise réponse") {
+      var message3 = "<div id='mauvRep'><strong>" + message2 + "<br> Votre pointage est " + messagePoint + ". </strong></div>"
+      affiche2.innerHTML=message3;
+    }
+
+ 
+ }
+
+
+
+ }
+
+
+
+
+
+  
 
 
